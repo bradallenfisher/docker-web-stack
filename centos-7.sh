becfa38fc0616451e99d58a15e10ccdc19f2be16
Brad Fisher <bradallenfisher@gmail.com>
2016-10-24 13:46:57 -0400

    Create centos-7.sh

---
+++ centos-7.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Docker Engine on RHEL Based Systems
+sudo yum update
+sudo curl -fsSL https://get.docker.com/ | sh
+sudo systemctl enable docker.service
+sudo systemctl start docker
+
+# Install Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.9.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose

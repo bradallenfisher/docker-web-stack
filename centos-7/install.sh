0e14096c1a192ab3308056af675ec2d879b72549
Brad Fisher <bradallenfisher@gmail.com>
2016-10-24 22:38:54 -0400

    Update install.sh

--- centos-7/install.sh
+++ centos-7/install.sh
@@ -12,5 +12,6 @@ sudo systemctl start docker
 # Install Docker Compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.9.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
 # Start the docker compose file
 source ../docker.sh

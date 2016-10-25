ea1bca7add9a943ef3c59632158f586e14d326c4
Brad Fisher <bradallenfisher@gmail.com>
2016-10-24 22:25:29 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,8 @@ os="$(cat /etc/*-release)"
 
 if [[ $os == *"NAME=\"CentOS Linux\""* && $os == *"VERSION=\"7"* ]]
 then
-  source centos-7.sh
+  source centos-7/centos-7.sh
+  docker-compose up -d
 else 
   echo "Docker Not Supported on this operating system"
 fi
